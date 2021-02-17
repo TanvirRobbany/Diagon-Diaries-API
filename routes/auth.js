3f9cb05285c20a9d1703133a4e1e7853b93ccf7e
TanvirRobbany <tanvirahmed80151@gmail.com>
2021-02-17 07:19:15 +0600

    Report Completed

--- routes/auth.js
+++ routes/auth.js
@@ -57,7 +57,7 @@ router.post('/', [
             }
         };
 
-        jwt.sign(payload, config.get('jwtSecret'), {expiresIn: 360000}, (err, token) => {
+        jwt.sign(payload, config.get('jwtSecret'), {expiresIn: 3600}, (err, token) => {
             if (err) throw err;
             res.json({token, isAdmin: user.isAdmin, userID: user._id, userName: user.name, userId: user.studentID});
         });
